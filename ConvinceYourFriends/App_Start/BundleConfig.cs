701261b77adb7de343cfea26ac0fd5da3b0ce17e
MattMcGowan <mattmcgowan1989@hotmail.com>
2015-10-30 01:08:03 +0000

    Namespace changes

--- ConvinceYourFriends/App_Start/BundleConfig.cs
+++ ConvinceYourFriends/App_Start/BundleConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web;
 using System.Web.Optimization;
 
-namespace ConvinceYourFriends
+namespace ConvinceYourFriends.Website
 {
     public class BundleConfig
     {
--- ConvinceYourFriends/Controllers/HomeController.cs
+++ ConvinceYourFriends/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace ConvinceYourFriends.Controllers
+namespace ConvinceYourFriends.Website.Controllers
 {
     public class HomeController : Controller
     {
